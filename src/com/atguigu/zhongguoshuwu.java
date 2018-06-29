9ac30cd8f96afa4a9267c1a3908ac87a7677e628
zhaohui <834159254@qq.com>
2018-06-30 00:09:13 +0800

    成功了

--- src/com/atguigu/zhongguoshuwu.java
+++ src/com/atguigu/zhongguoshuwu.java
@@ -2,8 +2,8 @@ package com.atguigu;
 
 public class zhongguoshuwu {
 	public static void main(String[] args) {
-		System.out.println("不容易啊");
-		System.out.println("不容易啊");
-		System.out.println("不容易啊");
+		System.out.println("成功了");
+		System.out.println("成功了");
+		System.out.println("成功了");
 	}
 }
