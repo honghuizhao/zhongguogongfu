11c532684f9e5271e399add5a42889047361a256
zhaohui <834159254@qq.com>
2018-06-29 23:59:41 +0800

    几度测试

--- src/com/atguigu/zhongguoshuwu.java
+++ src/com/atguigu/zhongguoshuwu.java
@@ -1,5 +1,9 @@
 package com.atguigu;
 
 public class zhongguoshuwu {
-
+	public static void main(String[] args) {
+		System.out.println("不容易啊");
+		System.out.println("不容易啊");
+		System.out.println("不容易啊");
+	}
 }
